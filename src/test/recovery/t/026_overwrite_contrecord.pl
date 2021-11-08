db1a3010728639b18855d69b43df4d5c828ca076
Alvaro Herrera <alvherre@alvh.no-ip.org>
2021-11-08 08:52:48 -0300

    Fix typo
    Introduced in e0eaeafd6646.
    
    Co-authored-by: Alexander Lakhin <exclusion@gmail.com>
    Discussion: https://postgr.es/m/83641f59-d566-b33e-ef21-a272a98675aa@gmail.com

--- src/test/recovery/t/026_overwrite_contrecord.pl
+++ src/test/recovery/t/026_overwrite_contrecord.pl
@@ -107,7 +107,7 @@ sleep 5;
 my $log = slurp_file($node_standby->logfile);
 like(
 	$log,
-	qr[sucessfully skipped missing contrecord at],
+	qr[successfully skipped missing contrecord at],
 	"found log line in standby");
 
 $node_standby->stop;
