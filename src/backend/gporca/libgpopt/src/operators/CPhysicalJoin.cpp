25adb269a37e445aa21fe034c6062868be9e4459
Richy Wang <wangxuqi@users.noreply.github.com>
2023-09-06 09:45:26 +0800

    Revert "Correcting derived distribution spec for CPhysicalJoin. (#16342)"
    This reverts commit 5c36a44ab03c43e82ef08006ad6021773c6176b6.

--- src/backend/gporca/libgpopt/src/operators/CPhysicalJoin.cpp
+++ src/backend/gporca/libgpopt/src/operators/CPhysicalJoin.cpp
@@ -419,13 +419,11 @@ CPhysicalJoin::PdsDerive(CMemoryPool *mp, CExpressionHandle &exprhdl) const
 
 	CDistributionSpec *pds;
 
-	if ((CDistributionSpec::EdtStrictReplicated == pdsOuter->Edt() ||
+	if (CDistributionSpec::EdtStrictReplicated == pdsOuter->Edt() ||
 		CDistributionSpec::EdtTaintedReplicated == pdsOuter->Edt() ||
-		CDistributionSpec::EdtUniversal == pdsOuter->Edt()) &&
-		CDistributionSpec::EdtUniversal != pdsInner->Edt())
+		CDistributionSpec::EdtUniversal == pdsOuter->Edt())
 	{
-		// if outer is replicated/universal and inner is not universal
-		// then return inner distribution
+		// if outer is replicated/universal, return inner distribution
 		pds = pdsInner;
 	}
 	else
